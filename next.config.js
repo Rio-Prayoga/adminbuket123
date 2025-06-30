38e4bfc6370b1c85813e075ae6f9167347505e10
Rio-Prayoga <riopray38@gmail.com>
2025-06-30 23:48:08 +0700

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export',
+  // Hapus output export karena tidak bisa untuk dynamic route
+  // output: 'export',
+
   eslint: {
     ignoreDuringBuilds: true,
   },
-  images: { unoptimized: true },
+  images: {
+    unoptimized: true,
+  },
 };
 
 module.exports = nextConfig;
